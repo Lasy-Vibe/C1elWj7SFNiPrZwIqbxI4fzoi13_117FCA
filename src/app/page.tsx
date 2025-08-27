e5fab93c853c494598ff592e2cf60b72af8f9231
MrIagoLasy <iago@lasy.ai>
2025-08-27 09:03:18 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@ import { ThemeToggle } from "@/components/ui/theme-toggle";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-4">
-      <h1 className="text-4xl font-bold">Aooooooooba!</h1>
+      <h1 className="text-8xl font-bold">A<span className="text-primary">oooooooo</span>ba!</h1>
       <ThemeToggle />
     </div>
   )
 
 }
+
